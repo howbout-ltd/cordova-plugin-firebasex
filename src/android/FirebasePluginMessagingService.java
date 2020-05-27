d49eee89d373b12947d91a1473ef5a75b1c8ce2d
Duncan Cowan <duncan@zyng.app>
2020-05-27 17:52:16 +0100

    Added ability to show notifications as MessageStyle on Android Must be a data notification and should contain the following values in the data array: notification_conversation_title: The title for the conversation notification_conversation_messages: The messages of the conversation in a JSON encoded array where each message is an object containing message, timestamp, sender

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -21,6 +21,10 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Map;
 import java.util.Random;
 
@@ -89,6 +93,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
             String messageType;
             String title = null;
             String body = null;
+            String tag = null;
             String id = null;
             String sound = null;
             String vibrate = null;
@@ -102,6 +107,9 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
 
             Map<String, String> data = remoteMessage.getData();
 
+            String conversationTitle = null;
+            JSONArray conversationMessages = null;
+
             if (remoteMessage.getNotification() != null) {
                 // Notification message payload
                 Log.i(TAG, "Received message: notification");
@@ -110,6 +118,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
                 RemoteMessage.Notification notification = remoteMessage.getNotification();
                 title = notification.getTitle();
                 body = notification.getBody();
+                tag = notification.getTag();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     channelId = notification.getChannelId();
                 }
@@ -128,6 +137,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
                 }
                 if(data.containsKey("notification_title")) title = data.get("notification_title");
                 if(data.containsKey("notification_body")) body = data.get("notification_body");
+                if(data.containsKey("notification_tag")) body = data.get("notification_tag");
                 if(data.containsKey("notification_android_channel_id")) channelId = data.get("notification_android_channel_id");
                 if(data.containsKey("notification_android_id")) id = data.get("notification_android_id");
                 if(data.containsKey("notification_android_sound")) sound = data.get("notification_android_sound");
@@ -137,6 +147,11 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
                 if(data.containsKey("notification_android_icon")) icon = data.get("notification_android_icon");
                 if(data.containsKey("notification_android_visibility")) visibility = data.get("notification_android_visibility");
                 if(data.containsKey("notification_android_priority")) priority = data.get("notification_android_priority");
+                if(data.containsKey("notification_conversation_title")) conversationTitle = data.get("notification_conversation_title");
+                try {
+                    if (data.containsKey("notification_conversation_messages")) conversationMessages = new JSONArray(data.get("notification_conversation_messages"));
+                } catch (JSONException e) {}
+
             }
 
             if (TextUtils.isEmpty(id)) {
@@ -149,6 +164,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Id: " + id);
             Log.d(TAG, "Title: " + title);
             Log.d(TAG, "Body: " + body);
+            Log.d(TAG, "Tag: " + tag);
             Log.d(TAG, "Sound: " + sound);
             Log.d(TAG, "Vibrate: " + vibrate);
             Log.d(TAG, "Light: " + light);
@@ -161,15 +177,15 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
 
             if (!TextUtils.isEmpty(body) || !TextUtils.isEmpty(title) || (data != null && !data.isEmpty())) {
                 boolean showNotification = (FirebasePlugin.inBackground() || !FirebasePlugin.hasNotificationsCallback() || foregroundNotification) && (!TextUtils.isEmpty(body) || !TextUtils.isEmpty(title));
-                sendMessage(remoteMessage, data, messageType, id, title, body, showNotification, sound, vibrate, light, color, icon, channelId, priority, visibility);
+                sendMessage(remoteMessage, data, messageType, id, title, body, tag, showNotification, sound, vibrate, light, color, icon, channelId, priority, visibility, conversationTitle, conversationMessages);
             }
         }catch (Exception e){
             FirebasePlugin.handleExceptionWithoutContext(e);
         }
     }
 
-    private void sendMessage(RemoteMessage remoteMessage, Map<String, String> data, String messageType, String id, String title, String body, boolean showNotification, String sound, String vibrate, String light, String color, String icon, String channelId, String priority, String visibility) {
-        Log.d(TAG, "sendMessage(): messageType="+messageType+"; showNotification="+showNotification+"; id="+id+"; title="+title+"; body="+body+"; sound="+sound+"; vibrate="+vibrate+"; light="+light+"; color="+color+"; icon="+icon+"; channel="+channelId+"; data="+data.toString());
+    private void sendMessage(RemoteMessage remoteMessage, Map<String, String> data, String messageType, String id, String title, String body, String tag, boolean showNotification, String sound, String vibrate, String light, String color, String icon, String channelId, String priority, String visibility, String conversationTitle, JSONArray conversationMessages) {
+        Log.d(TAG, "sendMessage(): messageType="+messageType+"; showNotification="+showNotification+"; id="+id+"; title="+title+"; body="+body+"; tag="+tag+"; sound="+sound+"; vibrate="+vibrate+"; light="+light+"; color="+color+"; icon="+icon+"; channel="+channelId+"; data="+data.toString());
         Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
             bundle.putString(key, data.get(key));
@@ -178,6 +194,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
         this.putKVInBundle("id", id, bundle);
         this.putKVInBundle("title", title, bundle);
         this.putKVInBundle("body", body, bundle);
+        this.putKVInBundle("tag", tag, bundle);
         this.putKVInBundle("sound", sound, bundle);
         this.putKVInBundle("vibrate", vibrate, bundle);
         this.putKVInBundle("light", light, bundle);
@@ -207,11 +224,30 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
 
             NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
             notificationBuilder
-                    .setContentTitle(title)
-                    .setContentText(body)
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(body))
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent);
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+            if (conversationMessages == null) {
+                notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(body));
+            } else {
+
+                // Process conversation messages
+                NotificationCompat.MessagingStyle notificationMessage = new NotificationCompat.MessagingStyle("Reply");
+                for (int i = 0; i < conversationMessages.length(); i++) {
+                    try {
+                        JSONObject messageInfo = conversationMessages.getJSONObject(i);
+                        String messageContent = messageInfo.getString("message");
+                        Long messageTime = messageInfo.getLong("timestamp");
+                        String messageSender = messageInfo.getString("sender");
+                        notificationMessage.addMessage(messageContent, messageTime, messageSender);
+                    } catch (JSONException e) {}
+                }
+                if (conversationTitle != null) notificationMessage.setConversationTitle(conversationTitle);
+                notificationBuilder.setStyle(notificationMessage);
+
+            }
 
             // On Android O+ the sound/lights/vibration are determined by the channel ID
             if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
@@ -334,7 +370,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
             // Display notification
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             Log.d(TAG, "show notification: "+notification.toString());
-            notificationManager.notify(id.hashCode(), notification);
+            notificationManager.notify(tag, id.hashCode(), notification);
         }
         // Send to plugin
         FirebasePlugin.sendMessage(bundle, this.getApplicationContext());
